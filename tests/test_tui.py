c5d99d75696a7a5f52666dcb4d2e2ef01dfd565f
Lundy Bernard <lundy.bernard@gmail.com>
2026-01-18 17:47:54 -0800

    spellcheck

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -28,6 +28,6 @@ async def test_welcome_message() -> None:
     tui = BatConfApp()
     async with tui.run_test() as _:
         assert tui.is_running
-        # check that mesage is in the DOM
+        # check that message is in the DOM
         msg = tui.query_one('#welcome-message')
         assert str(msg.render()) == 'Welcome to BatConf TUI'
